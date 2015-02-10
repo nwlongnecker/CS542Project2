a27cd48f95e23134e5d7756f7fc68eaf1f88a4f7
Mike Calder <mrcalder@wpi.edu>
2015-02-10 13:38:51 -0500

    Initial Implementation of Put

---
+++ src/valuestore/ValueStoreImpl.java
@@ -0,0 +1,55 @@
+package valuestore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ValueStoreImpl implements IValueStore
+{
+	private final String databaseFolder;
+	
+	public ValueStoreImpl()
+	{
+		this("database");
+	}
+
+	public ValueStoreImpl(String folder)
+	{	
+		databaseFolder = folder + "/";
+		
+		// Make the database directory.
+		File databaseDir = new File(folder);
+		databaseDir.mkdir();
+	}
+	
+	@Override
+	public void put(int key, byte[] data)
+	{
+		File dataFile = new File(databaseFolder + key);
+		dataFile.delete();
+		
+		try (FileOutputStream fileWriter = new FileOutputStream(databaseFolder + key))
+		{
+			fileWriter.write(data);
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public byte[] get(int key)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(int key)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
