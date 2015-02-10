b4aa2bf25776f113eafcb6001032ea134d66627e
Nathan <nathanlongnecker@gmail.com>
2015-02-10 15:26:59 -0500

    Add tests for put

--- test/valuestore/ValueStoreImplTests.java
+++ test/valuestore/ValueStoreImplTests.java
@@ -1,9 +1,12 @@
 package valuestore;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -46,8 +49,9 @@ public class ValueStoreImplTests {
 
 	@Test
 	public void testPutNewFile() throws FileNotFoundException, IOException {
+		// Test that put creates a new file with specific contents
 		String dirName = "test1";
-		ValueStoreImpl vs = new ValueStoreImpl("test1");
+		ValueStoreImpl vs = new ValueStoreImpl(dirName);
 		
 		int key = 1;
 		File newFile = new File(dirName + "/" + key);
@@ -57,14 +61,114 @@ public class ValueStoreImplTests {
 		}
 		assertFalse(newFile.exists());
 		
-		vs.put(key, "Hello".getBytes());
+		byte[] inData = "Hello".getBytes();
+		
+		vs.put(key, inData);
 		
 		assertTrue(newFile.exists());
 		
-//		try(FileReader file = new FileReader("" + key)) {
-//			byte[] read
-//			assertEquals("Hello", file.r)
-//		}
+		// Create a byte array to store the data from the file.
+		int dataLength = (int) newFile.length();
+		byte[] outData = new byte[dataLength];
+		
+		// Get a reader for the file containing the data.
+		try (FileInputStream fileReader = new FileInputStream(dirName + "/" + key))
+		{
+			// Read in the bytes of the file.
+			fileReader.read(outData);
+		}
+		
+		assertTrue(Arrays.equals(inData, outData));
+		
+		assertTrue(newFile.delete());
+	}
+	
+	@Test
+	public void testPutExistingFile() throws FileNotFoundException, IOException {
+		// Test that put replaces an existing file
+		String dirName = "test2";
+		ValueStoreImpl vs = new ValueStoreImpl(dirName);
+		
+		int key = 1;
+		File newFile = new File(dirName + "/" + key);
+		// If the file already exists, delete it
+		if(newFile.exists()) {
+			assertTrue(newFile.delete());
+			assertFalse(newFile.exists());
+		}
+		assertTrue(newFile.createNewFile());
+		
+		byte[] inData = "Hello2".getBytes();
+		
+		vs.put(key, inData);
+		
+		assertTrue(newFile.exists());
+		
+		// Create a byte array to store the data from the file.
+		int dataLength = (int) newFile.length();
+		byte[] outData = new byte[dataLength];
+		
+		// Get a reader for the file containing the data.
+		try (FileInputStream fileReader = new FileInputStream(dirName + "/" + key))
+		{
+			// Read in the bytes of the file.
+			fileReader.read(outData);
+		}
+		
+		assertTrue(Arrays.equals(inData, outData));
+		
+		assertTrue(newFile.delete());
+	}
+	
+	@Test
+	public void testPutNonemptyExistingFile() throws FileNotFoundException, IOException {
+		// Test that put completely replaces the contents of a file
+		String dirName = "test3";
+		ValueStoreImpl vs = new ValueStoreImpl(dirName);
+		
+		int key = 1;
+		File newFile = new File(dirName + "/" + key);
+		// If the file already exists, delete it
+		if(newFile.exists()) {
+			assertTrue(newFile.delete());
+			assertFalse(newFile.exists());
+		}
+		byte[] startData = "LongDataAlreadyInFile".getBytes();
+		try (FileOutputStream fileWriter = new FileOutputStream(dirName + "/" + key))
+		{
+			// Write the data to the new empty file.
+			fileWriter.write(startData);
+		}
+		
+		int dataLength = (int) newFile.length();
+		byte[] initData = new byte[dataLength];
+		
+		// Get a reader for the file containing the data.
+		try (FileInputStream fileReader = new FileInputStream(dirName + "/" + key))
+		{
+			// Read in the bytes of the file.
+			fileReader.read(initData);
+		}
+		
+		assertTrue(Arrays.equals(initData, startData));
+		
+		byte[] inData = "Hello2".getBytes();
+		
+		vs.put(key, inData);
+		
+		assertTrue(newFile.exists());
+		
+		dataLength = (int) newFile.length();
+		byte[] outData = new byte[dataLength];
+		
+		// Get a reader for the file containing the data.
+		try (FileInputStream fileReader = new FileInputStream(dirName + "/" + key))
+		{
+			// Read in the bytes of the file.
+			fileReader.read(outData);
+		}
+		
+		assertTrue(Arrays.equals(inData, outData));
 		
 		assertTrue(newFile.delete());
 	}
