b23707c30bfc980617e2631d3fa0c83d8da67e63
Nathan Longnecker <nathanlongnecker@gmail.com>
2015-02-27 15:56:22 -0500

    Add implementation to Bucket

--- src/indexmechanism/Bucket.java
+++ src/indexmechanism/Bucket.java
@@ -18,6 +18,9 @@ public class Bucket {
 	 */
 	public static final int MAX_BUCKET_SIZE = 4;
 	
+	/**
+	 * The next bucket in the linked list of indices
+	 */
 	private Bucket nextBucket;
 
 	/**
@@ -26,22 +29,46 @@ public class Bucket {
 	 */
 	public Bucket() {
 		set = new HashSet<Index>();
+		nextBucket = null;
 	}
 	
 	/**
-	 * If the bucket is not full, add the specified index to the bucket and return true. Otherwise, return false;
-	 * @param toAdd The index to add to the bucket.
-	 * @return Returns true if the index was added, false otherwise.
+	 * If the bucket is not full, add the specified index to the bucket.
+	 * Otherwise, puts it in the next bucket in the chain.
+	 * @param toAdd The index to add to the bucket chain.
 	 */
 	public void add(Index toAdd) {
 		if (set.size() < MAX_BUCKET_SIZE) {
 			set.add(toAdd);
+		} else if (nextBucket == null) {
+			nextBucket = new Bucket();
+			nextBucket.add(toAdd);
+		} else {
+			nextBucket.add(toAdd);
 		}
 	}
 	
 	/**
-	 * Removes the given key from the bucket if it exists
-	 * @param key The key to removed from the bucket
+	 * Gets the Index associated with the dataValue from the bucket chain.
+	 * @param dataValue The dataValue of the index to get from this bucket chain.
+	 * @return Returns true if the index was successfully removed.
+	 */
+	public Index get(String dataValue) {
+		for(Index i : set) {
+			if (i.getDataValue().equals(dataValue)) {
+				return i;
+			}
+		}
+		if (nextBucket != null) {
+			return nextBucket.get(dataValue);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Removes the given key from the bucket chain if it exists
+	 * @param key The key to remove from the bucket chain
 	 * @return Returns true if the index was successfully removed.
 	 */
 	public boolean remove(String key) {
@@ -54,13 +81,19 @@ public class Bucket {
 		}
 		if (toRemove != null) {
 			return set.remove(toRemove);;
+		} else if (nextBucket != null) {
+			return nextBucket.remove(key);
 		} else {
 			return false;
 		}
 	}
 	
-	public boolean isFull() {
-		return false;
+	/**
+	 * Checks whether the bucket chain is overflowing.
+	 * @return Returns true if this bucket chain is overflowing.
+	 */
+	public boolean hasOverflow() {
+		return nextBucket != null;
 	}
 	
 	public Bucket split(int order) {
